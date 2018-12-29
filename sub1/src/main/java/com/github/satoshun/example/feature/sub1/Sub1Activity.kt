eba41de71c492cf9924469584fd3b581a75545e7
SatoShun <shun.sato1@gmail.com>
2018-12-29 08:11:12 +0000

    format code

--- sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1Activity.kt
+++ sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1Activity.kt
@@ -5,20 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class Sub1Activity : AppCompatActivity(),
   Sub1Contract.View {
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     val presenter = object : Sub1Contract.Presenter {}
   }
-
-  override fun showPage() {
-  }
-}
-
-internal interface Sub1Contract {
-  interface View {
-    fun showPage()
-  }
-
-  interface Presenter
 }
