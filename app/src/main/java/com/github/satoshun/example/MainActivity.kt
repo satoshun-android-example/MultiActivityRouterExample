4cb559d23ca4bae3db8d4920caa87d7885450434
SatoShun <shun.sato1@gmail.com>
2018-12-29 06:57:59 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/example/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.github.satoshun.example
+
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main_act)
+  }
+}
