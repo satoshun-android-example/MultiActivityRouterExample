d05c1068cf8a3d696599bc8085057471cd3299f5
SatoShun <shun.sato1@gmail.com>
2018-12-29 11:14:53 +0000

    Sub1RouterImpl to internal

--- sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1Builder.kt
+++ sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1Builder.kt
@@ -2,8 +2,16 @@ package com.github.satoshun.example.feature.sub1
 
 import dagger.Binds
 import dagger.Module
+import dagger.android.ContributesAndroidInjector
+
+@Module(
+  includes = [Sub1ActivityModule::class]
+)
+interface Sub1Builder
 
 @Module
-interface Sub1Builder {
+internal interface Sub1ActivityModule {
+  @ContributesAndroidInjector fun contributeSub1Activity(): Sub1Activity
+
   @Binds fun bindSub1Router(impl: Sub1RouterImpl): Sub1Router
 }
--- sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1RouterImpl.kt
+++ sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1RouterImpl.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import javax.inject.Inject
 
-class Sub1RouterImpl @Inject constructor() : Sub1Router {
+internal class Sub1RouterImpl @Inject constructor() : Sub1Router {
   override fun routeToSub1(context: Context): Intent {
     return Intent(context, Sub1Activity::class.java)
   }
