b49b6f5066feca484edc6ea3df71d9d874f1018f
SatoShun <shun.sato1@gmail.com>
2018-12-30 01:45:19 +0000

    add back to button

--- sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1Activity.kt
+++ sub1/src/main/java/com/github/satoshun/example/feature/sub1/Sub1Activity.kt
@@ -2,12 +2,15 @@ package com.github.satoshun.example.feature.sub1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.github.satoshun.example.feature.main.MainRouter
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.sub1_act.*
 import javax.inject.Inject
 
 class Sub1Activity : AppCompatActivity(),
   Sub1Contract.View {
+
+  @Inject lateinit var router: MainRouter
 
   override fun onCreate(savedInstanceState: Bundle?) {
     AndroidInjection.inject(this)
@@ -17,6 +20,8 @@ class Sub1Activity : AppCompatActivity(),
     val presenter = object : Sub1Contract.Presenter {}
 
     button.setOnClickListener {
+      val intent = router.routeToMain(this)
+      startActivity(intent)
     }
   }
 }
